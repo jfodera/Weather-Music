80e8c5a4d0bf4fbf84c725c534ad1a835c9b868a
Joseph <josephfodera25@gmail.com>
2024-10-03 19:52:47 -0400

    s

--- lab3/resources/insertToDB.php
+++ lab3/resources/insertToDB.php
@@ -7,17 +7,19 @@
 
       $dbOk = false;
 
-      /* Create a new database connection object, passing in the host, username,
+      /* Create a new database connection object, passing in the host, username, 
      password, and database to use. The "@" suppresses errors. */
       @$db = new mysqli('localhost', 'phpmyadmin', '!fodAdmin!', 'jsonObjs');
 
       //connect_error is a property of a mysqli object that has any error message if it false
       if ($db->connect_error) {
-         echo 'Could not connect to the database. Error: ';
-         //errno gives the error code 
-         echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
-      } else {
-         $dbOk = true;
+         $connectErrors = array(
+           'errors' => true,
+           //error number 
+           'errno' => mysqli_connect_errno(),
+           'error' => mysqli_connect_error()
+         );
+         echo json_encode($connectErrors);
       }
       
 
--- lab3/resources/lab3.js
+++ lab3/resources/lab3.js
@@ -106,8 +106,6 @@ $(document).ready(function() {
          getPlays(auth,searchTerm)
    
          .then(function(spotJSON){
-            console.log(vals[1]);
-            console.log(spotJSON);
             //remember, relative to page running on 
             //Makes post request to php file, basically sends it the JSON 
             fetch("resources/insertToDB.php", {
@@ -121,14 +119,14 @@ $(document).ready(function() {
    
             })
 
-            // //Catches echo response if there is an error
-            // .then(function(response){
-            //    //must do this because returns a promise
-            //    return(response.text());
-            // })
-            // .then(function(error){
-            //    console.log(error);
-            // })
+            //Catches echo response if there is an error
+            .then(function(response){
+               //must do this because returns a promise
+               return(response.json());
+            })
+            .then(function(error){
+               console.log(error.error);
+            })
    
          })
       });
