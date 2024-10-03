a23cbab9949c56515ea51aa11be72bfe797acdb6
Joseph <josephfodera25@gmail.com>
2024-10-03 19:44:30 -0400

    Update insertToDB.php

--- lab3/resources/insertToDB.php
+++ lab3/resources/insertToDB.php
@@ -5,20 +5,20 @@
       //the php:// makes it so that it reads raw data from the post requests body 
       $data = file_get_contents("php://input");
 
-   //    $dbOk = false;
+      $dbOk = false;
 
-   //    /* Create a new database connection object, passing in the host, username,
-   //   password, and database to use. The "@" suppresses errors. */
-   //    @$db = new mysqli('localhost', 'phpmyadmin', '!fodAdmin!', 'jsonObjs');
+      /* Create a new database connection object, passing in the host, username,
+     password, and database to use. The "@" suppresses errors. */
+      @$db = new mysqli('localhost', 'phpmyadmin', '!fodAdmin!', 'jsonObjs');
 
-   //    //connect_error is a property of a mysqli object that has any error message if it false
-   //    if ($db->connect_error) {
-   //       echo 'Could not connect to the database. Error: ';
-   //       //errno gives the error code 
-   //       echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
-   //    } else {
-   //       $dbOk = true;
-   //    }
+      //connect_error is a property of a mysqli object that has any error message if it false
+      if ($db->connect_error) {
+         echo 'Could not connect to the database. Error: ';
+         //errno gives the error code 
+         echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+      } else {
+         $dbOk = true;
+      }
       
 
    }
