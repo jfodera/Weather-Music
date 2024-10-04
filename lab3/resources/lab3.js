e5423cc9f12086cb9702b4ecdf45504a761539cc
Joseph <josephfodera25@gmail.com>
2024-10-04 10:21:42 -0400

    Update lab3.js

--- lab3/resources/lab3.js
+++ lab3/resources/lab3.js
@@ -122,7 +122,7 @@ async function apiBut(){
 
    console.log(plays)
 
-   // const weathID = await insert(weatherData); 
+   const weathID = await insert(weatherData); 
    const playsID = await insert(plays);
 
    // console.log(weathID);
