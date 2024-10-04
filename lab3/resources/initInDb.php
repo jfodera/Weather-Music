94f67ed1a232590dda69dc5913b05137a9226048
Joseph <josephfodera25@gmail.com>
2024-10-04 11:00:54 -0400

    Update initInDb.php

--- lab3/resources/initInDb.php
+++ lab3/resources/initInDb.php
@@ -4,43 +4,54 @@
    if(isset($_POST)){
       //the php:// makes it so that it reads raw data from the post requests body 
       $data = file_get_contents("php://input");
-
+      // $data = "text's";
       //single quote escape chars: 
       //Get ID
       //error: must put escape chars around the single quotes quotes 
-
-
-      /* Create a new database connection object, passing in the host, username, 
-     password, and database to use. */
-      $db = new mysqli('localhost', 'phpmyadmin', '!fodAdmin!', 'apiDat');
-
-      //connect_error is a property of a mysqli object that has any error message if it false
-      if ($db->connect_error) {
-         //can echo back in json as well using json_encode
-         echo "connection failed:" . $db->connect_error;
+      $wEsc = "";
+      for($i = 0; $i < strlen($data); $i++){
+         if($data[$i] == "'"){
+            // Need double escape here
+            $wEsc = $wEsc . "\\'";
+         }else{
+            $wEsc = $wEsc . $data[$i];
+         }
       }
-      //Prepare and insert API's val to database
-      $type = "misc";
-      $insQuery = "insert into jsonObjs (`jdoc`,`apiType`) values(?,?)";
-      $statement = $db->prepare($insQuery);
-      $statement->bind_param("ss", $data, $type);
-      $statement->execute();
+
+      echo $wEsc;
+   }
+
+//       /* Create a new database connection object, passing in the host, username, 
+//      password, and database to use. */
+//       $db = new mysqli('localhost', 'phpmyadmin', '!fodAdmin!', 'apiDat');
+
+//       //connect_error is a property of a mysqli object that has any error message if it false
+//       if ($db->connect_error) {
+//          //can echo back in json as well using json_encode
+//          echo "connection failed:" . $db->connect_error;
+//       }
+//       //Prepare and insert API's val to database
+//       $type = "misc";
+//       $insQuery = "insert into jsonObjs (`jdoc`,`apiType`) values(?,?)";
+//       $statement = $db->prepare($insQuery);
+//       $statement->bind_param("ss", $data, $type);
+//       $statement->execute();
 
 
 
 
-      $query = "SELECT * FROM jsonObjs WHERE jdoc='$data'";
-      echo $query;
-      // $result = $db->query($query);
-      // $record = $result->fetch_assoc();
-      // $id = $record['callId'];
+//       $query = "SELECT * FROM jsonObjs WHERE jdoc='$data'";
+//       echo $query;
+//       // $result = $db->query($query);
+//       // $record = $result->fetch_assoc();
+//       // $id = $record['callId'];
 
-      // echo $id; 
+//       // echo $id; 
       
    
       
-   }
+//    }
 
-   $db->close();
+//    $db->close();
 
 ?>
