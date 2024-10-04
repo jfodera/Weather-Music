8562072ddbc31bbef9862db464d6fd0670b350c9
Joseph <josephfodera25@gmail.com>
2024-10-03 22:28:20 -0400

    Update index.php

--- lab3/index.php
+++ lab3/index.php
@@ -21,9 +21,9 @@
    $query = 'SELECT * FROM jsonObjs WHERE apiType="weather"';
    $result = $db->query($query);
    $record = $result->fetch_assoc();
-   $weathAr = json_decode($record['jdoc']);
+   $weathAr = json_decode($record['jdoc'], true);
    // print_r($weathAr);
-   echo $weathAr->name;
+   echo $weathAr['name'];
 
 ?>
 
