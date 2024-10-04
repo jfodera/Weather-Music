d987154e569b03c4eee70207297ec87a0df3bd3a
Joseph <josephfodera25@gmail.com>
2024-10-03 21:50:26 -0400

    Update insertToDB.php

--- lab3/resources/insertToDB.php
+++ lab3/resources/insertToDB.php
@@ -20,7 +20,7 @@
       }
       // //don't need prepare statement here because not a form submission 
       $type = "weather";
-      $insQuery = "insert into jsonObjs (`jdoc`, `apiType') values(?,?)";
+      $insQuery = "insert into jsonObjs (`jdoc`, `apiType`) values(?,?);";
       $statement = $db->prepare($insQuery);
       $statement->bind_param("ss", $data, $type);
       $statement->execute();
