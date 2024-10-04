b561dec8b9920267ab88b719ea50f15874dbcc2e
Joseph <josephfodera25@gmail.com>
2024-10-04 11:46:52 -0400

    Update index.php

--- lab3/index.php
+++ lab3/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href= "resources/lab3.css">
+    <link rel="icon" href="resources/back.jpg" >
     <title>RPI Weather & Music</title>
   </head>
   <body>
