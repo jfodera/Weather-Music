a198c10ce736ea9cd344105c1c8dc7a51b78e347
Joseph <josephfodera25@gmail.com>
2024-10-03 20:03:14 -0400

    Update insertToDB.php

--- lab3/resources/insertToDB.php
+++ lab3/resources/insertToDB.php
@@ -7,17 +7,15 @@
 
       $dbOk = false;
 
-      echo "con faile:";
-
       /* Create a new database connection object, passing in the host, username, 
      password, and database to use. */
-      // $db = new mysqli('localhost', 'phpmyadmin', '!fodAdmin!', 'jsonObjs');
+      $db = new mysqli('localhost', 'phpmyadmin', '!fodAdmin!', 'jsonObjs');
 
-      // //connect_error is a property of a mysqli object that has any error message if it false
-      // if ($db->connect_error) {
+      //connect_error is a property of a mysqli object that has any error message if it false
+      if ($db->connect_error) {
   
-      //    echo "con faile:" . $db->connect_error;
-      // }
+         echo "con faile:" .$db->connect_error;
+      }
       
 
    }
