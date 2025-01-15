1da10463aa4005316a6a1f0e87940c6fa80ae5e9
Joseph <josephfodera25@gmail.com>
2025-01-15 11:59:51 -0500

    Added a break

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>RPI Weather & Music</title>
   </head>
    <body>
-      I've updated my resume and replaced this project with an even better one called Finder! 
+      I've updated my resume and replaced this project with an even better one called Finder! </br>
       <a href='https://finder.eastus.cloudapp.azure.com/finder'>Click Here </a> to go to my new project Finder! </br>
       <a href='https://github.com/jfodera/Finder'>Click Here </a> to view it's github with tester login credentials in the README!
  </body>
